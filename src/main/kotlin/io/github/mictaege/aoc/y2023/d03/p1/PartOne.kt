7eafecfb7a4b3affc70a2b9281f5b096943796c6
Michael Taege <mictaege@googlemail.com>
2023-12-03 12:04:10 +0100

    20231203

---
+++ src/main/kotlin/io/github/mictaege/aoc/y2023/d03/p1/PartOne.kt
@@ -0,0 +1,74 @@
+package io.github.mictaege.aoc.y2023.d03.p1
+
+import io.github.mictaege.aoc.y2023.d03.input
+
+data class Entry(val x: Int, val y: Int, val value: Char)
+
+class Part(schema: Schema, val entries: List<Entry>) {
+    val number = entries.map { "${it.value}" }.reduce { a, b -> a + b }.toInt()
+
+    private val minY = entries.minOf { it.y }
+    private val maxY = entries.maxOf { it.y }
+    private val minX = entries.minOf { it.x }
+    private val maxX = entries.maxOf { it.x }
+    val min = entries.first { it.x == minX && it.y == minY }
+    val max = entries.last() { it.x == maxX && it.y == maxY }
+
+    val neighbours = schema.neighbours(this)
+
+    val adjacentToSymbol = neighbours.map { it.value }.any { it != '.' && !it.isDigit()  }
+}
+
+class Schema(val original: String) {
+    val entries: List<Entry>
+    val parts: List<Part>
+    init {
+        val _entries = mutableListOf<Entry>()
+        original.lines().forEachIndexed { y, line ->
+            line.toCharArray().forEachIndexed { x, char ->
+                _entries.add(Entry(x, y, char))
+            }
+        }
+        entries = _entries
+
+        val _parts = mutableListOf<Part>()
+        original.lines().forEachIndexed { y, line ->
+            val partEntries = mutableListOf<Entry>()
+            val lineEntries = mutableListOf<Entry>()
+            line.toCharArray().forEachIndexed { x, char ->
+                lineEntries.add(Entry(x, y, char))
+            }
+            lineEntries.forEach {
+                if (it.value.isDigit()) {
+                    partEntries.add(it)
+                } else {
+                    if (partEntries.isNotEmpty()) {
+                        _parts.add(Part(this, partEntries))
+                    }
+                    partEntries.clear()
+                }
+            }
+            if (partEntries.isNotEmpty()) {
+                _parts.add(Part(this, partEntries))
+            }
+        }
+
+        parts = _parts
+    }
+
+    fun neighbours(part: Part): List<Entry> {
+        return entries.asSequence()
+            .filter { it.y >= part.min.y - 1 }
+            .filter { it.x >= part.min.x - 1 }
+            .filter { it.y <= part.max.y + 1 }
+            .filter { it.x <= part.max.x + 1 }
+            .filter { it !in part.entries }
+            .toList()
+    }
+
+}
+
+fun main() {
+    val result = Schema(input).parts.filter { it.adjacentToSymbol }.sumOf { it.number }
+    println(result)
+}
