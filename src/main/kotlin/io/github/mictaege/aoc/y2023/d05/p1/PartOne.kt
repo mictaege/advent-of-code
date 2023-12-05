b5561cceb7082f299e39b93e24eb9ff03ec850da
Michael Taege <mictaege@googlemail.com>
2023-12-05 09:15:02 +0100

    20231205

--- src/main/kotlin/io/github/mictaege/aoc/y2023/d05/p1/PartOne.kt
+++ src/main/kotlin/io/github/mictaege/aoc/y2023/d05/p1/PartOne.kt
@@ -1,25 +1,20 @@
 package io.github.mictaege.aoc.y2023.d05.p1
 
+import io.github.mictaege.aoc.y2023.d05.example
 import io.github.mictaege.aoc.y2023.d05.input
 
 class Seed(val value: Long) {
     var location = 0L
-    val visited = mutableListOf<String>()
 }
 
 class MappingRange(val original: String) {
     val rangeLength = original.split(Regex("\\s+"))[2].toLong()
     val srcStart = original.split(Regex("\\s+"))[1].toLong()
+    val srcEnd = srcStart + rangeLength
     val destStart = original.split(Regex("\\s+"))[0].toLong()
-    val srcToDestMap: Map<Long, Long>
-
-    init {
-        srcToDestMap = mutableMapOf()
-        for (i in 0.. rangeLength) {
-            srcToDestMap.put(srcStart + i, destStart + i)
-        }
-    }
-
+    val destEnd = srcStart + rangeLength
+    val diff = srcStart - destStart
+    fun matchingDest(start: Long) = start - diff
 }
 
 class Mapping(val original: String) {
@@ -27,11 +22,9 @@ class Mapping(val original: String) {
     val destination: String = original.split(":")[0].split(Regex("\\s+"))[0].split("-")[2]
     val ranges: List<MappingRange> = original.split(":")[1].trim().lines().map { l -> MappingRange(l) }
 
-
     fun mapSrcToDest(src: Long): Long {
-        return ranges.find { it.srcToDestMap.containsKey(src) }?.srcToDestMap?.get(src) ?: src
+        return ranges.find { src in it.srcStart .. it.srcEnd  }?.matchingDest(src) ?: src
     }
-
 }
 
 class Conversion(val original: String) {
@@ -49,27 +42,24 @@ class Conversion(val original: String) {
             val mapping = Mapping(sections[i])
             mappings.put(mapping.source, mapping)
         }
-        
+
         seeds.forEach {
             var srcType = "seed"
             var src = it.value
-            while (!it.visited.contains(srcType) && mappings.containsKey(srcType)) {
-                it.visited.add(srcType)
+            while (mappings.containsKey(srcType)) {
                 val mapping = mappings[srcType]
                 src = mapping!!.mapSrcToDest(src)
                 srcType = mapping.destination
                 if (srcType == "location") {
-                   it.location = src
+                    it.location = src
                 }
-            }   
+            }
         }
+
     }
+
 }
 
 fun main() {
     println(Conversion(input).lowestLocation)
-    //Conversion(example).seeds.forEach {
-    //    println("${it.value} -> ${it.location}")
-    //}
-    //println(Conversion(example).lowestLocation)
 }
