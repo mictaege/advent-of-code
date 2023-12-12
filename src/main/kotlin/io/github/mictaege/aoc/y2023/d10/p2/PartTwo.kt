7ef881690b66d35b0c4c6c9f6dde4a7d0dba0e8e
Michael Taege <mictaege@googlemail.com>
2023-12-12 07:50:03 +0100

    20231210

--- src/main/kotlin/io/github/mictaege/aoc/y2023/d10/p2/PartTwo.kt
+++ src/main/kotlin/io/github/mictaege/aoc/y2023/d10/p2/PartTwo.kt
@@ -1,6 +1,7 @@
 package io.github.mictaege.aoc.y2023.d10.p2
 
 import io.github.mictaege.aoc.y2023.d10.example_3
+import io.github.mictaege.aoc.y2023.d10.input
 import io.github.mictaege.aoc.y2023.d10.p2.Direction.*
 
 enum class Direction {
@@ -34,18 +35,44 @@ data class Pos(val y: Int, val x: Int)
 class Cell(val pos: Pos, val char: Char) {
     val tile = Tile.from(char)
 
-    fun reachesBorder(table: Table): Boolean {
-        // test none border cells that are able to reach the border
-        return table.isBorder(this)
+    fun findNeighbour(table: Table): Cell? {
+        val up: Cell? = table.cells[Pos(pos.y - 1, pos.x)]
+        val left: Cell? = table.cells[Pos(pos.y, pos.x - 1)]
+        val down: Cell? = table.cells[Pos(pos.y + 1, pos.x)]
+        val right: Cell? = table.cells[Pos(pos.y, pos.x + 1)]
+
+        if (up != null && up !in table.steps && tile.hasDirection(NORTH) && up.tile.hasDirection(SOUTH)) {
+            return up
+        } else if (left != null && left !in table.steps && tile.hasDirection(WEST) && left.tile.hasDirection(EAST)) {
+            return left
+        } else if (down != null && down !in table.steps && tile.hasDirection(SOUTH) && down.tile.hasDirection(NORTH)) {
+            return down
+        }  else if (right != null && right !in table.steps && tile.hasDirection(EAST) && right.tile.hasDirection(WEST)) {
+            return right
+        } else {
+            return null
+        }
+    }
+
+    fun isInterior(table: Table): Boolean {
+        if (tile != Tile.GROUND) {
+            return false
+        } else {
+            val x_Axis = (0..table.max_X).toList().map { Pos(pos.y, it) }.filterNot { it == pos }
+            val intersections = x_Axis.filter { it -> it in table.steps.map { c -> c.pos } }.size
+            return intersections == 0 || intersections % 2 != 0
+        }
     }
 
 }
 
 class Table(val original: String) {
     val cells: Map<Pos, Cell>
-    val size_Y: Int
-    val size_X: Int
-    val caught: Int
+    val max_Y: Int
+    val max_X: Int
+    val start: Cell
+    val steps: List<Cell>
+    val interiorCells: List<Cell>
 
     init {
         cells = mutableMapOf()
@@ -57,19 +84,22 @@ class Table(val original: String) {
                 cells.put(pos, Cell(pos, c))
             }
         }
-        size_Y = cells.values.map { it.pos.y }.max()
-        size_X = cells.values.map { it.pos.x }.max()
-
-        caught = cells.values.filter { it.tile == Tile.GROUND }.filter { !it.reachesBorder(this) }.size
+        max_Y = cells.keys.map { it.y }.max()
+        max_X = cells.keys.map { it.x }.max()
+        start = cells.values.first { it.tile == Tile.START }
+        steps = mutableListOf()
+        steps.add(start)
+        var cell = start.findNeighbour(this)
+        while (cell != null) {
+            steps.add(cell)
+            cell = cell.findNeighbour(this)
+        }
+        interiorCells = cells.values.filter { it.isInterior(this) }
 
     }
-
-    fun isBorder(cell: Cell) = cell.pos.y == 0 || cell.pos.y == size_Y || cell.pos.x == 0 || cell.pos.x == size_X
-
 }
 
 fun main() {
-    // currently 17 (not 4) 
-    val result = Table(example_3).caught
+    val result = Table(example_3).interiorCells.size
     println(result)
 }
