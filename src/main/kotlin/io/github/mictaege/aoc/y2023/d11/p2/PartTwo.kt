4963e706906a46cc504593ce29331c821167e400
Michael Taege <mictaege@googlemail.com>
2023-12-13 15:18:20 +0100

    20231211_2

--- src/main/kotlin/io/github/mictaege/aoc/y2023/d11/p2/PartTwo.kt
+++ src/main/kotlin/io/github/mictaege/aoc/y2023/d11/p2/PartTwo.kt
@@ -1,7 +1,6 @@
-package io.github.mictaege.aoc.y2023.d11.p1
+package io.github.mictaege.aoc.y2023.d11.p2
 
 import io.github.mictaege.aoc.y2023.cartesianProduct
-import io.github.mictaege.aoc.y2023.d11.example
 import io.github.mictaege.aoc.y2023.d11.input
 import kotlin.math.abs
 import kotlin.math.max
@@ -30,7 +29,7 @@ class Route(val a: Sector, val b: Sector) {
     val min_X = min(a.pos.x, b.pos.x)
 }
 
-class Universe(val original: String) {
+class Universe(val original: String, val expFactor: Long) {
     val sectors: MutableMap<Pos, Sector> = mutableMapOf()
     val max_Y
         get() = sectors.keys.map { it.y }.max()
@@ -39,7 +38,7 @@ class Universe(val original: String) {
     val emptyColumns: Set<Int>
     val emptyRows: Set<Int>
     val routes: List<Route>
-    val overallDistance: Int
+    val overallDistance: Long
 
     init {
         val lines = original.lines()
@@ -74,16 +73,18 @@ class Universe(val original: String) {
 
         routes = sectors.values.filter { it.type == Type.GALAXY }.cartesianProduct().map { Route(it.first, it.second) }
 
-        overallDistance = routes.sumOf { distance(it) }
+        overallDistance = routes.sumOf { distance(it, expFactor) }
     }
 
-    private fun distance(route: Route): Int {
+    fun distance(route: Route, expFactor: Long): Long {
         val emptyColumnsBetween = (route.min_X + 1..<route.max_X).toSet().intersect(emptyColumns).count()
         val emptyRowsBetween = (route.min_Y + 1..<route.max_Y).toSet().intersect(emptyRows).count()
+        val x_offset = emptyColumnsBetween * max(expFactor - 1, 1)
+        val y_offset = emptyRowsBetween * max(expFactor - 1, 1)
         val x_1 = route.min_X
-        val x_2 = route.max_X + emptyColumnsBetween
+        val x_2 = route.max_X + x_offset
         val y_1 = route.min_Y
-        val y_2 = route.max_Y + emptyRowsBetween
+        val y_2 = route.max_Y + y_offset
         return abs(x_1 - x_2) + abs(y_1 - y_2)
     }
 
@@ -96,6 +97,6 @@ class Universe(val original: String) {
 }
 
 fun main() {
-    val result = Universe(input).overallDistance
+    val result = Universe(input, 1000000).overallDistance
     println(result)
 }
