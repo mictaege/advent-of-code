e688adfa073f74467b8ce7bdd06e8207fb8f467c
Michael Taege <mictaege@googlemail.com>
2023-12-15 06:52:02 +0100

    20231212_1 wip

--- src/main/kotlin/io/github/mictaege/aoc/y2023/d12/p1/PartOne.kt
+++ src/main/kotlin/io/github/mictaege/aoc/y2023/d12/p1/PartOne.kt
@@ -1,11 +1,27 @@
 package io.github.mictaege.aoc.y2023.d12.p1
 
 import io.github.mictaege.aoc.y2023.d12.example
+import io.github.mictaege.aoc.y2023.d12.p1.Condition.UNKNOWN
 
-class DamagedGroup(val length: Int)
+class Possibilities(val springs: List<Spring>) {
+    override fun toString(): String {
+        return springs.joinToString { it.toString() }
+    }
+}
 
-class Rule(val original: String) {
-    val damagedGroups = original.split(",").map { DamagedGroup(it.toInt()) }
+class Group(val length: Int)
+
+class Rule(val original: String, val springs: List<Spring>) {
+    val group = original.split(",").map { Group(it.toInt()) }
+    val unknown = springs.filter { it.condition == UNKNOWN }.size
+    val operational = unknown - group.size - 1
+    val damaged = unknown - operational
+    val possibilities = mutableListOf<Possibilities>()
+
+    init {
+        
+    }
+
 }
 
 enum class Condition(val char: Char) {
@@ -17,18 +33,29 @@ enum class Condition(val char: Char) {
         fun from(char: Char) = Condition.entries.toTypedArray().first { it.char == char }
     }
 
+    override fun toString(): String {
+        return "$char"
+    }
+
+
 }
-data class Spring(val condition: Condition)
+data class Spring(val condition: Condition) {
+    override fun toString(): String {
+        return "$condition"
+    }
+}
 
 class Record(val original: String) {
     val springs = original.split(" ")[0].toCharArray().map { Spring(Condition.from(it)) }
-    val rule = Rule(original.split(" ")[1])
+    val rule = Rule(original.split(" ")[1], springs)
+
 }
 
 class Report(val original: String) {
     val records = original.lines().map { Record(it) }
+    val sum = records.sumOf { it.rule.possibilities.size }
 }
 
 fun main() {
-    Report(example)
+    Report(example).records.forEach { println(it.rule) }
 }
